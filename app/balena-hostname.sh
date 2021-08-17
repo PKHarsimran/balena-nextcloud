942a9b825ff19526d5aa6cbd6a2ec5fc91f1f130
Harsimran sidhu <22066581+PKHarsimran@users.noreply.github.com>
2021-08-17 09:30:26 -0400

    Add files via upload

--- app/balena-hostname.sh
+++ app/balena-hostname.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-
+DEVLOC=/dev/sda2
 # openFleets: configure hostname
 curl -X PATCH --header "Content-Type:application/json" \
     --data "{\"network\": {\"hostname\": \"$BALENA_HOSTNAME\"}}" \
     "$BALENA_SUPERVISOR_ADDRESS/v1/device/host-config?apikey=$BALENA_SUPERVISOR_API_KEY"
+
+echo " Mounting $DEVLOC"
+mkdir /mnt/sda2
+mount $DEVLOC /mnt/sda2
+chmod 707 /mnt/sda2
+ls -al
 
 exec /entrypoint.sh php-fpm
